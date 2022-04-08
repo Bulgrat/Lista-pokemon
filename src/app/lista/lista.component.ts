570d41ab73b4b169a9248a8d0efcb59def632313
Walter S L Junior <wjunino10@hotmail.com>
2022-04-08 18:23:15 -0300

    teste mat-select

--- src/app/lista/lista.component.ts
+++ src/app/lista/lista.component.ts
@@ -9,11 +9,15 @@ import { PokemonService } from '../service/pokemon.service';
 export class ListaComponent implements OnInit {
 
   branco = 'https://www.colorhexa.com/d3d3d3.png';
+  pokemon: string;
 
   constructor(public pokemonService: PokemonService) { }
 
   mudarPkm(valor) {
     this.pokemonService.onePoke(valor)
+  }
+  mudarPkm1() {
+    this.pokemonService.onePoke(this.pokemon)
   }
   ngOnInit(): void { }
 }
