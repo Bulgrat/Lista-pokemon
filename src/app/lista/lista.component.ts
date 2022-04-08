2c4ec458930299be487d8a9d7f23e096392cb80c
walter da silva luiz junior <wjunino10@hotmail.com>
2022-04-08 03:11:18 -0300

    Fonte/ background/ style card

--- src/app/lista/lista.component.ts
+++ src/app/lista/lista.component.ts
@@ -8,12 +8,12 @@ import { PokemonService } from '../service/pokemon.service';
 })
 export class ListaComponent implements OnInit {
 
-  branco = 'https://5287aa00874a313e299d-1850966fc307ff23e1e789aeafd2476b.ssl.cf5.rackcdn.com/PostImagem/42240/foto-branca-download-wallpapers-e-onde-usar_o1fcm9aiph1mte1km1f11cs53mng.png';
+  branco = 'https://www.colorhexa.com/d3d3d3.png';
 
   constructor(public pokemonService: PokemonService) { }
 
-  mudarPkm(val){
-    this.pokemonService.onePoke(val)
+  mudarPkm(valor) {
+    this.pokemonService.onePoke(valor)
   }
-  ngOnInit(): void {}
+  ngOnInit(): void { }
 }
