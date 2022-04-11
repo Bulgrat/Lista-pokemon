a1be7e2fb0310e6490cde2390a6294ec4a295e30
Walter S L Junior <wjunino10@hotmail.com>
2022-04-11 18:46:08 -0300

    Novo serviço

--- src/app/lista/lista.component.ts
+++ src/app/lista/lista.component.ts
@@ -4,32 +4,43 @@ import { PokemonService } from '../service/pokemon.service';
 @Component({
   selector: 'app-lista',
   templateUrl: './lista.component.html',
-  styleUrls: ['./lista.component.css']
+  styleUrls: ['./lista.component.css'],
 })
 export class ListaComponent implements OnInit {
-
   branco = 'https://www.colorhexa.com/d3d3d3.png';
   oneP: any = {};
   pokemons: any = '';
-  pokemon: string;
+  // pokemon: string;
+  type: any = {};
 
-  constructor(public pokemonService: PokemonService) { }
+  constructor(public pokemonService: PokemonService) {}
 
   ngOnInit(): void {
     this.obterPokemons();
   }
+
   mudarPkm(event) {
     console.table(event);
-    this.pokemonService.onePoke(event)
-    .pipe().subscribe(poke =>{
-      this.oneP = poke;
-      console.table(this.oneP);
-    });
+    this.pokemonService
+      .onePokeS(event)
+      .pipe()
+      .subscribe((poke) => {
+        this.oneP = poke;
+        this.type = poke.types;
+        // this.pokemonService.typeS(this.type).pipe().subscribe(type =>{
+
+        // }
+        // );
+        // console.table(this.type);
+      });
   }
-  private obterPokemons(): void{
-    this.pokemonService.obterPokemon()
-    .pipe().subscribe(pokemons =>{
-      this.pokemons = pokemons.results;
-    });
+
+  private obterPokemons(): void {
+    this.pokemonService
+      .obterPokemonS()
+      .pipe()
+      .subscribe((pokemons) => {
+        this.pokemons = pokemons.results;
+      });
   }
 }
--- src/app/service/pokemon.service.ts
+++ src/app/service/pokemon.service.ts
@@ -1,40 +1,31 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError, Observable, throwError } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService {
 
-  poke: any = {};
+  // poke: any = {};
+  // typ: any = {};
 
   constructor(private httpClient: HttpClient) {
 
-    // this.pokemons();
   }
-  // https://pokeapi.co/api/v2/type/normal
-  // async pokemons() {
-  //   const requisicao = await this.httpClient
-  //   .get<any>('https://pokeapi.co/api/v2/pokemon?limit=9999')
-  //   .toPromise();
-  //   this.poke = requisicao.results;
-  // }
-  obterPokemon():Observable<any> {
+  obterPokemonS():Observable<any> {
     return this.httpClient
     .get<any>('https://pokeapi.co/api/v2/pokemon?limit=9999')
     .pipe();
   }
-
-  // async onePoke(param) {
-  //   const request = await this.httpClient
-  //   .get<any>(`https://pokeapi.co/api/v2/pokemon/${param}`)
-  //   .toPromise();
-  //   this.oneP = request;
-  // }
-  onePoke(param) {
+  onePokeS(param) {
     return this.httpClient
     .get<any>(`https://pokeapi.co/api/v2/pokemon/${param}`)
+    .pipe();
+  }
+  typeS(tipo){
+    return this.httpClient.get<any>(tipo)
     .pipe();
   }
 }
