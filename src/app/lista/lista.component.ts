8ae87915ea5bbea4fd4f872aecf01b465f47e18a
Walter S L Junior <wjunino10@hotmail.com>
2022-04-20 18:32:35 -0300

    teste new build

--- src/app/lista/lista.component.ts
+++ src/app/lista/lista.component.ts
@@ -10,8 +10,8 @@ export class ListaComponent implements OnInit {
   branco = 'https://www.colorhexa.com/d3d3d3.png';
   oneP: any = {};
   pokemons: any = '';
+  types: any = {};
   // pokemon: string;
-  type: any = {};
 
   constructor(public pokemonService: PokemonService) {}
 
@@ -20,18 +20,21 @@ export class ListaComponent implements OnInit {
   }
 
   mudarPkm(event) {
-    console.table(event);
     this.pokemonService
       .onePokeS(event)
       .pipe()
       .subscribe((poke) => {
         this.oneP = poke;
-        this.type = poke.types;
-        // this.pokemonService.typeS(this.type).pipe().subscribe(type =>{
+        this.types = poke.types;
+        poke.types.forEach(tipo => {
+          this.pokemonService.typeS(this.types).pipe().subscribe(type =>{
 
-        // }
-        // );
-        // console.table(this.type);
+          });
+        });
+        // this.pokemonService.typeS(this.types).pipe().subscribe(type =>{
+
+        // });
+        console.table(this.types[0].type.url);
       });
   }
 
--- src/app/service/pokemon.service.ts
+++ src/app/service/pokemon.service.ts
@@ -1,30 +1,27 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
-import { Observable} from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PokemonService {
-
   // poke: any = {};
   // typ: any = {};
 
-  constructor(private httpClient: HttpClient) {
-  }
-  obterPokemonS():Observable<any> {
+  constructor(private httpClient: HttpClient) {}
+  obterPokemonS(): Observable<any> {
     return this.httpClient
-    .get<any>('https://pokeapi.co/api/v2/pokemon?limit=9999')
-    .pipe();
+      .get<any>('https://pokeapi.co/api/v2/pokemon?limit=9999')
+      .pipe();
   }
   onePokeS(param) {
     return this.httpClient
-    .get<any>(`https://pokeapi.co/api/v2/pokemon/${param}`)
-    .pipe();
+      .get<any>(`https://pokeapi.co/api/v2/pokemon/${param}`)
+      .pipe();
   }
-  typeS(tipo){
-    return this.httpClient.get<any>(tipo)
-    .pipe();
+  typeS(tipo) {
+    return this.httpClient.get<any>(tipo).pipe();
   }
 }
