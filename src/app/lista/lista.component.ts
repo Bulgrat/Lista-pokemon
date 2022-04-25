1bd8857733afb20b7a2d9ce48856318c5a82e3bb
Walter S L Junior <wjunino10@hotmail.com>
2022-04-25 14:01:06 -0300

    teste

--- src/app/lista/lista.component.ts
+++ src/app/lista/lista.component.ts
@@ -28,11 +28,9 @@ export class ListaComponent implements OnInit {
         this.types = poke.types;
         poke.types.forEach(tipo => {
           this.pokemonService.typeS(this.types).pipe().subscribe(type =>{
-
           });
         });
         // this.pokemonService.typeS(this.types).pipe().subscribe(type =>{
-
         // });
         console.table(this.types[0].type.url);
       });
